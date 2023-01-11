cea056fd072cf281231dc459800ff126c9469484
AV_Ivanov <90529554+cufrosso@users.noreply.github.com>
2023-01-12 01:47:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <canvas id="canvas" width="600" height="400">
+      <script src="serv.js"></script>
+    </canvas>
+  </body>
+</html>
